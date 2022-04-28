0e15bbde7c3ac7676d9310fb198c56242ed53d4b
Git-mlne <201805971@post.au.dk>
2022-04-28 14:39:21 +0200

    Navigation_bar

--- app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/TripActivity.java
+++ app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/TripActivity.java
@@ -1,14 +1,48 @@
 package dk.au.mad22spring.group19.appproject_travlers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class TripActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+//Reference: https://www.geeksforgeeks.org/bottom-navigation-bar-in-android/
+//Reference: https://www.youtube.com/watch?v=Bb8SgfI4Cm4
+public class TripActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+
+         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+         bottomNavigationView.setSelectedItemId(R.id.home);
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+                return true;
+
+            case R.id.location:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new MapFragment()).commit();
+                return true;
+
+            case R.id.person:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new ProfileFragment()).commit();
+                return true;
+        }
+        return false;
     }
 }
