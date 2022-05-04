58c8826308cf623fd52184d142360d1374f4451b
Nprimdal <201811526@post.au.dk>
2022-05-04 17:33:04 +0200

    Login and Register activity added

--- app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/LoginActivity.java
+++ app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/LoginActivity.java
@@ -1,33 +1,93 @@
 package dk.au.mad22spring.group19.appproject_travlers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TAG = "AUTH";
+    // UI widgets
+    private EditText edtEmail;
+    private EditText edtPassword;
     private Button btnLogin;
+    private TextView txtCreateAccount;
+    
+    //Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.bntLogin);
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Login();
+                //login();
+                goToSecurePart();
             }
         });
+        txtCreateAccount = findViewById(R.id.txtCreateAccount);
+        txtCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { goToCreateNewAccount(); }
+        });
+        
+        
     }
 
-    private void Login() {
-        Intent intent = new Intent(this, TripActivity.class);
-        startActivity(intent);
+
+    private void login() {
+    String email = edtEmail.getText().toString();
+    String password = edtPassword.getText().toString();
+    if (email==null || email.length()<1||password==null||password.length()<1){
+        Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+        return;
+    }
+    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if(task.isSuccessful()){
+                Log.d(TAG, "Login successful");
+                goToSecurePart();
+            }
+            else{
+                Log.d(TAG, "Login failed: ", task.getException());
+                Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_LONG).show();
+            }
+        }
+    });
+    }
+
+    private void goToCreateNewAccount() {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+
+    }
+
+    private void goToSecurePart(){
+        Intent i = new Intent(this, TripActivity.class);
+        startActivity(i);
         finish();
     }
 
--- app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/RegisterActivity.java
+++ app/src/main/java/dk/au/mad22spring/group19/appproject_travlers/RegisterActivity.java
@@ -1,14 +1,108 @@
 package dk.au.mad22spring.group19.appproject_travlers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final String TAG = "AUTH";
+    //Ui widgets
+    private EditText edtFullName;
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnCreateNewAccount;
+
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private static final String USER = "user";
+    private User _user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtFullName = findViewById(R.id.edtCreateNewName);
+        edtEmail = findViewById(R.id.edtCreateNewEmail);
+        edtPassword = findViewById(R.id.edtCreateNewPassword);
+        btnCreateNewAccount = findViewById(R.id.btnCreateNewAccount);
+        btnCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                if(email==null||email.length()<1||password==null||password.length()<1){
+                    Toast.makeText(RegisterActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String fullName = edtFullName.getText().toString();
+                _user = new User(email, password, fullName);
+                createNewAccount(email, password);
+            }
+        });
+
+        //database = FirebaseDatabase.getInstance();
+        //mDatabase = database.getReference(USER);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void createNewAccount(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)       //call to create a new user and set callbacks
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Account created");
+                            Toast.makeText(RegisterActivity.this, "Account created!", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            goToLogin();
+                        } else {
+                            Log.d(TAG, "Could not create account", task.getException());
+                            Toast.makeText(RegisterActivity.this, "FAILED to create account", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void goToLogin() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void updateUI(FirebaseUser user) {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().
+                setDisplayName(_user.getFullName()).
+                build();
+
+        user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "User profile updated.");
+                }
+            }
+        });
     }
 }
